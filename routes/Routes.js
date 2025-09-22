1f7ec901a69fae5e70e39184f088ac450a2f08cf
homeshoppr <homeshoppr247@gmail.com>
2025-09-22 14:18:08 +0530

    update

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import fs from "fs";
+import Papa from "papaparse";
+
 
 import { userauth } from '../middleware/loginauth.js';
 // import { departmentdeletefunc, d } from '../controller/Category.js';
@@ -19,6 +22,11 @@ router.get('/getAllCategory',GetCategoryfunc)
 router.get('/singleCategory/:id',GetSingleCategory)
 router.post ('/CategoryUpdate/:id',CategoryUpdateFunc)
 router.delete('/categorydelete/:id',Categorydeletefunc)
+router.get("/api/meta-metrics", (req, res) => {
+  const file = fs.readFileSync("data/meta_mom_sample.csv", "utf8");
+  const parsed = Papa.parse(file, { header: true, dynamicTyping: true });
+  res.json(parsed.data);
+});
 // department routes ending here 
 
 export default router;
